d2d99cb935bd9e6fba34d23f439ff56c5fa56f69
Valentin Knyshov <v.a.knishov@bk.ru>
2026-07-05 19:16:20 +0300

    HW: Add test and coverage config

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,75 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+  test("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  test("Books names should be sorted from reverse order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Волшебник изумрудного города",
+      "Властелин Колец",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  test("Books names with same values should stay unchanged", () => {
+    const input = [
+      "Властелин Колец", 
+      "Гарри Поттер", 
+      "Гарри Поттер",
+    ];
+
+    const expected = [
+      "Властелин Колец", 
+      "Гарри Поттер", 
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  test("Already sorted books should remain unchanged", () => {
+    const input = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
   });
 });
